19b963d36478951cf5f51d837f43aa9141cdc69a
Сергей <nerub3@rambler.ru>
2024-11-13 22:39:41 +0300

    Создание класса 2 и заполнение класса 1

--- class1.cs
+++ class1.cs
@@ -5,11 +5,16 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string myName;
-        myName = "Jane";
+        const string MyName = "Jane";
 
-        Console.WriteLine(myName);
+        Console.WriteLine(MyName);
 
+        Console.WriteLine("\t Привет Мир");
+        Console.WriteLine("\t Мне 27 лет");
+        Console.WriteLine("\t My name is \n {0}", MyName);
+        Console.WriteLine("\u0040");
+        Console.ReadLine("\x23");
         Console.ReadKey();
+
     }
 }
